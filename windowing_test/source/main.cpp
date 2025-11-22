725048d45b5dba47ca5a05ea08b8597f263de510
Khalil Henoud <khalilhenoud@gmail.com>
2025-11-22 15:13:22 -0500

    fixes

--- windowing_test/source/main.cpp
+++ windowing_test/source/main.cpp
@@ -1,21 +1,27 @@
 /**
  * @file main.cpp
  * @author khalilhenoud@gmail.com
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-01-03
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include <stdint.h>
 #include <windowing/windowing.h>
 
 
+void update(void)
+{}
+
 int main(int argc, char *argv[])
 {
-  window_data_t data = create_window("test_window_setup", "test window setup");
-  uint64_t result = handle_message_loop_blocking();
+  window_data_t data = create_window(
+    "test_window_setup",
+    "test window setup",
+    320, 200);
+  uint64_t result = handle_message_loop_blocking(update);
   destroy_window(&data);
   return result;
 }
